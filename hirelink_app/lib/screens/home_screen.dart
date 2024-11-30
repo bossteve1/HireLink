85de561ff84fdc54c1b3746cbe943159e18fb5b5
bossteve1 <bossteve98@gmail.com>
2024-11-30 15:53:14 +0300

    add home screen file

---
+++ hirelink_app/lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'job_post_screen.dart';
+import 'candidate_list_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Job Portal')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => JobPostScreen()),
+                );
+              },
+              child: Text('Post a Job'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CandidateListScreen()),
+                );
+              },
+              child: Text('View Candidates'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

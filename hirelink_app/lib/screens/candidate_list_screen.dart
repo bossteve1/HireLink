8f2753fc50c23732a8d06dc10eade3f3362ef7ea
bossteve1 <bossteve98@gmail.com>
2024-11-30 15:59:29 +0300

    add candidate list screen

---
+++ hirelink_app/lib/screens/candidate_list_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/candidate_provider.dart';
+import '../widgets/candidate_card.dart';
+
+class CandidateListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final candidates = Provider.of<CandidateProvider>(context).candidates;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Candidates List')),
+      body: ListView.builder(
+        itemCount: candidates.length,
+        itemBuilder: (ctx, index) => CandidateCard(candidate: candidates[index]),
+      ),
+    );
+  }
+}

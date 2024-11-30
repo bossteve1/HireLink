d2b20aa1353a26614573a3709e3eb6481c53c6a9
bossteve1 <bossteve98@gmail.com>
2024-11-30 15:39:56 +0300

    add candidate file to models

---
+++ hirelink_app/lib/models/candidate.dart
@@ -0,0 +1,15 @@
+class Candidate {
+  final String id;
+  final String name;
+  final String email;
+  final List<String> skills;
+  final String resumeUrl;
+
+  Candidate({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.skills,
+    required this.resumeUrl,
+  });
+}
